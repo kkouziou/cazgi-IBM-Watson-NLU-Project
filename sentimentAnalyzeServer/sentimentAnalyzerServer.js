2e0cd124645fff373cde09196f414201df302c93
Konstantinos Kouziou <k.kouziou@gmail.com>
2021-05-12 10:18:02 +0000

    NLU added

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bp = require('body-parser')
 dotenv.config();
 
 function getNLUInstance(){
@@ -26,25 +27,110 @@ app.use(express.static('client'))
 const cors_app = require('cors');
 app.use(cors_app());
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.get("/",(req,res)=>{
     res.render('index.html');
   });
 
 app.get("/url/emotion", (req,res) => {
+    const analyzeParams = {
+        'url' : req.query.url,
+        'features': {
+        'emotion': {
+            }
+        }       
+    };
 
-    return res.send({"happy":"90","sad":"10"});
+    const nlu = getNLUInstance();
+
+    nlu.analyze(analyzeParams)
+        .then(analysisResults => {
+            emotion = analysisResults.result.emotion.document.emotion
+            // emotion = JSON.stringify(analysisResults.result.emotion.document.emotion, null, 2)
+            // temp = JSON.stringify(analysisResults, null, 2)
+            console.log(emotion);
+            return res.send(emotion);
+        })
+        .catch(err => {
+            console.log('error:', err);
+            return res.send('error');
+        });
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    const analyzeParams = {
+        'url' : req.query.url,
+        'features': {
+        'sentiment': {
+            }
+        }       
+    };
+
+    const nlu = getNLUInstance();
+
+    nlu.analyze(analyzeParams)
+        .then(analysisResults => {
+            sentiment = analysisResults.result.sentiment.document
+            console.log(sentiment);
+            return res.send(sentiment);
+        })
+        .catch(err => {
+            console.log('error:', err);
+            return res.send('error');
+        });
+    
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    const analyzeParams = {
+        'text' : req.query.text,
+        'features': {
+        'emotion': {
+            }
+        }       
+    };
+
+    const nlu = getNLUInstance();
+
+    nlu.analyze(analyzeParams)
+        .then(analysisResults => {
+            emotion = analysisResults.result.emotion.document.emotion
+            // emotion = JSON.stringify(analysisResults.result.emotion.document.emotion, null, 2)
+            // temp = JSON.stringify(analysisResults, null, 2)
+            console.log(emotion);
+            return res.send(emotion);
+        })
+        .catch(err => {
+            console.log('error:', err);
+            return res.send('error');
+        });
+    
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    const analyzeParams = {
+        'text' : req.query.text,
+        'features': {
+        'sentiment': {
+            }
+        }       
+    };
+
+    const nlu = getNLUInstance();
+
+    nlu.analyze(analyzeParams)
+        .then(analysisResults => {
+            sentiment = analysisResults.result.sentiment.document
+            console.log(sentiment);
+            return res.send(sentiment);
+        })
+        .catch(err => {
+            console.log('error:', err);
+            return res.send('error');
+        });
+    
 });
 
 let server = app.listen(8080, () => {
